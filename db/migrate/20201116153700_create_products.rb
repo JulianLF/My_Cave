1add556d82175bdb5291a882f48246efb7a50d13
Julian Lacher <jlacherfougere@gmail.com>
2020-11-16 18:53:46 +0100

    fixing user registration and model + fixing db migration + remove 2nd navbar

--- db/migrate/20201116153700_create_products.rb
+++ db/migrate/20201116153700_create_products.rb
@@ -2,10 +2,10 @@ class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
       t.string :name
-      t.references :category_id, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
       t.text :description
       t.integer :price
-      t.references :user_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.boolean :availability, default: true
 
       t.timestamps
