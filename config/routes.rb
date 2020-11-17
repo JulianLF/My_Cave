a2a80a2156310948e9a363ca41e4702e7be8c1ee
Julian Lacher <jlacherfougere@gmail.com>
2020-11-17 11:42:34 +0100

    add pundit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users do
-    resources :products, except: %i[index]
-  end
   root to: 'pages#home'
-  resources :products, only: %i[index show]
+  devise_for :users do
+    resources :products, except: %i[index new]
+  end
+  resources :products, except: [:new]
+  resources :carts, only: [:index]
+  resources :transactions, only: %i[index show]
 end
