56cc232cbc2cf4125fd312e715d3a97bce19472e
Alexis Roger <a.roger37@gmail.com>
2020-11-17 10:32:19 +0100

    added new schema

--- db/schema.rb
+++ db/schema.rb
@@ -76,9 +76,6 @@ ActiveRecord::Schema.define(version: 2020_11_16_155407) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "username", default: "", null: false
-    t.string "first_name", default: "", null: false
-    t.string "last_name", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -87,7 +84,6 @@ ActiveRecord::Schema.define(version: 2020_11_16_155407) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
