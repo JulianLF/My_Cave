ad715d08b2c100e914dbfdd32a118c5cec3d38de
Julian Lacher <jlacherfougere@gmail.com>
2020-11-19 14:37:12 +0100

    active storage for caegory & product images

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,13 +4,16 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :transactions
   has_many :carts
-  after_validation :default_image
+  after_save :default_image
 
   validates :name, presence: true
   validates :description, presence: true, length: { minimum: 10, maximum: 400 }
   validates :price, presence: true
 
   def default_image
-    self.photo = category.photo unless photo?
+    return if photo.presence
+
+    photo.attach(io: open("http://res.cloudinary.com/dsgm7fnvb/image/upload/c_fill,h_300,w_400/#{category.photo.key}"),
+                 filename: "#{name}.jpg")
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,47 +7,65 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Creating 3 new users"
 
-Chuck = User.new(username: "Chuck Norris", first_name: "Chuck", last_name: "Norris", email:"godlike@gmail.com", password: "iamsecurity", admin: true)
-JC = User.new(username: "JeanCastex75", first_name: "Jean", last_name: "Castex", email:"lovemanu@gmail.com", password: "manuforever")
-Heenok = User.new(username: "XxLeRoiHeenokxX", first_name: "Roi", last_name: "Heenok", email:"lovemyself@gmail.com", password: "meforever")
-Chuck.save
-JC.save
-Heenok.save
-puts "Jean Castex & Roi Heenok has been created"
+chuck = User.new(username: "Chuck Norris", first_name: "Chuck", last_name: "Norris", email:"godlike@gmail.com", password: "iamsecurity", admin: true)
+chuck.save
+print '.'
+jc = User.new(username: "JeanCastex75", first_name: "Jean", last_name: "Castex", email:"lovemanu@gmail.com", password: "manuforever")
+jc.save
+print '.'
+heenok = User.new(username: "XxLeRoiHeenokxX", first_name: "Roi", last_name: "Heenok", email:"lovemyself@gmail.com", password: "meforever")
+heenok.save
+print '.'
+
+puts "\nJean Castex & Roi Heenok has been created"
 puts "Chuck Norris is now an admin"
-puts "----------"
-puts "Creating 4 categories"
+puts "\n----------------------------"
+puts "\nCreating 4 categories"
 
-beer_category = Category.new(name: "beer", photo: "https://res.cloudinary.com/dykscnyvu/image/upload/v1605625374/josh-duncan-6Sa26Lsjtok-unsplash_y6qw6l.jpg")
-wine_category = Category.new(name: "wine", photo: "https://res.cloudinary.com/dykscnyvu/image/upload/v1605784215/wolfgang-hasselmann-GPwuH2q9mcs-unsplash_ivvsto.jpg")
-liquors_category = Category.new(name: "liquors", photo: "https://res.cloudinary.com/dykscnyvu/image/upload/v1605784372/liquors_j1ybtn.jpg")
-champagne_category = Category.new(name: "champagne", photo: "https://res.cloudinary.com/dykscnyvu/image/upload/v1605784471/kevin-kelly-PPneSBqfCCU-unsplash_ml7a9p.jpg")
-
+beer_category = Category.new(name: "beer")
 beer_category.save
+beer_category.photo.attach(io: open("https://res.cloudinary.com/dykscnyvu/image/upload/v1605625374/josh-duncan-6Sa26Lsjtok-unsplash_y6qw6l.jpg"), filename: 'beer.jpg')
+print '.'
+wine_category = Category.new(name: "wine")
 wine_category.save
+wine_category.photo.attach(io: open("https://res.cloudinary.com/dykscnyvu/image/upload/v1605784215/wolfgang-hasselmann-GPwuH2q9mcs-unsplash_ivvsto.jpg"), filename: 'wine.jpg')
+print '.'
+liquors_category = Category.new(name: "liquors")
 liquors_category.save
+liquors_category.photo.attach(io: open("https://res.cloudinary.com/dykscnyvu/image/upload/v1605784372/liquors_j1ybtn.jpg"), filename: 'liquors.jpg')
+print '.'
+champagne_category = Category.new(name: "champagne")
 champagne_category.save
+champagne_category.photo.attach(io: open("https://res.cloudinary.com/dykscnyvu/image/upload/v1605784471/kevin-kelly-PPneSBqfCCU-unsplash_ml7a9p.jpg"), filename: 'champagne.jpg')
+print '.'
 
-puts "Created beer, wine, liquors and champagne categories"
-puts "----------"
-puts "Creating 4 products"
+puts "\nCreated beer, wine, liquors and champagne categories"
+puts "\n----------------------------"
+puts "\nCreating 7 products"
 
-biere_de_mamie = Product.new(name: "Bière de ma grand mère", description: "Une bière faite avec amour, comme ma grand mère la faisait", price: 18, user_id: 3, category_id: 1)
-vin_de_noah = Product.new(name: "Vin de Noah", description: "Pour enfiler les chaussures à bascule", price: 12, user_id: 3, category_id: 2)
-champ = Product.new(name: "Le Champ à JC", description: "Pour les soirées avec Manu", price: 1280, user_id: 2, category_id: 4)
-sky = Product.new(name: "Le Sky à JC", description: "Pour les soirées avec Raoult", price: 120, user_id: 2, category_id: 4)
-rhum = Product.new(name: "Planteur maison", description: "Planteur de Guadeloupe", price: 15, user_id: 3, category_id: 4)
-cerise = Product.new(name: "Liqueur de cerise", description: "Liqueur maison à la cerise", price: 8, user_id: 3, category_id: 4)
-rhubarbe = Product.new(name: "Eau de vie de Rhubarbe", description: "Eau de vie Rhubarbe 65°", price: 12, user_id: 1, category_id: 4)
-
-biere_de_mamie.save
+biere_de_mamie = Product.new(name: "Bière de ma grand mère", description: "Une bière faite avec amour, comme ma grand mère la faisait", price: 18, user_id: heenok.id, category_id: beer_category.id)
+biere_de_mamie.save!
+print '.'
+vin_de_noah = Product.new(name: "Vin de Noah", description: "Pour enfiler les chaussures à bascule", price: 12, user_id: heenok.id, category_id: wine_category.id)
 vin_de_noah.save
+print '.'
+champ = Product.new(name: "Le Champ à JC", description: "Pour les soirées avec Manu", price: 1280, user_id: jc.id, category_id: champagne_category.id)
 champ.save
+print '.'
+sky = Product.new(name: "Le Sky à JC", description: "Pour les soirées avec Raoult", price: 120, user_id: jc.id, category_id: champagne_category.id)
 sky.save
+print '.'
+rhum = Product.new(name: "Planteur maison", description: "Planteur de Guadeloupe", price: 15, user_id: heenok.id, category_id: liquors_category.id)
 rhum.save
+print '.'
+cerise = Product.new(name: "Liqueur de cerise", description: "Liqueur maison à la cerise", price: 8, user_id: heenok.id, category_id: liquors_category.id)
 cerise.save
+print '.'
+rhubarbe = Product.new(name: "Eau de vie de Rhubarbe", description: "Eau de vie Rhubarbe 65°", price: 12, user_id: chuck.id, category_id: liquors_category.id)
 rhubarbe.save
+print '.'
 
-puts "Products created, ready to go!"
+
+puts "\nProducts created, ready to go!"
 
 
